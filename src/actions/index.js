87299eb1fb446fae9d08e7b23886c7397ea3dfc2
Marcelo De Lima <delima_marcelo@outlook.com>
2022-06-22 18:43:11 -0300

    Exercicio 01 Finalizado

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,13 @@
 // Coloque aqui suas actions
+export const EMAIL_ACTION = 'EMAIL_ACTION';
+export const WALLET_ACTION = 'WALLET_ACTION';
+
+export const emailAction = (email) => ({
+  type: EMAIL_ACTION,
+  email,
+});
+
+export const walletAction = (wallet) => ({
+  type: WALLET_ACTION,
+  wallet,
+});
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,94 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { emailAction } from '../actions';
 
 class Login extends React.Component {
+  state = {
+    email: '',
+    senha: '',
+    btn: true,
+  };
+
+  handleChange = ({ target }) => {
+    this.setState(
+      {
+        [target.id]: target.value,
+      },
+      () => this.validadeForm(),
+    );
+  };
+
+  // habilitaBtn = () => {
+  //   const { email, senha } = this.state;
+  //   const re = /\S+@\S+\.\S+/;
+  //   const MIN_SENHA = 5;
+  //   if (re.test(email) && senha.length > MIN_SENHA) {
+  //     this.setState({ btn: false });
+  //   }
+  // };
+
+  validadeForm = () => {
+    const SIX = 6;
+    const { email, senha } = this.state;
+    const mailFormat = /^\w+@[a-zA-Z_]+?.[a-zA-Z]{2,3}$/;
+    const emailValid = email.match(mailFormat);
+    const passwordValid = senha.length >= SIX;
+    this.setState({ btn: !(emailValid && passwordValid) });
+  };
+
+  walletPage = async () => {
+    const { history, userEmail } = this.props;
+    const { email } = this.state;
+    userEmail(email);
+    history.push('/carteira');
+  };
+
   render() {
-    return <div>Login</div>;
+    const { btn } = this.state;
+    return (
+      <div>
+        <h1>Login</h1>
+        <form>
+          <label htmlFor="email">
+            <input
+              id="email"
+              data-testid="email-input"
+              type="email"
+              placeholder="email"
+              onChange={ this.handleChange }
+            />
+          </label>
+          <label htmlFor="senha">
+            <input
+              id="senha"
+              data-testid="password-input"
+              type="password"
+              placeholder="senha"
+              minLength="6"
+              onChange={ this.handleChange }
+            />
+          </label>
+          <button type="button" disabled={ btn } onClick={ this.walletPage }>
+            {' '}
+            Entrar
+            {' '}
+          </button>
+        </form>
+      </div>
+    );
   }
 }
 
-export default Login;
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+  userEmail: PropTypes.func.isRequired,
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  userEmail: (email) => dispatch(emailAction(email)),
+});
+
+export default connect(null, mapDispatchToProps)(Login);
